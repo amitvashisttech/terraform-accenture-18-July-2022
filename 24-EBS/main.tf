e901b749d31cefa06d9792794fd96582eeb1666d
Amit Vashist <amitvashist7@outlook.com>
2022-07-22 04:34:43 +0000

    26-Route53

---
+++ 24-EBS/main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "dev-app" { 
+  ami = data.aws_ami.myami.id
+  #ami = "ami-05803413c51f242b7"
+  instance_type = lookup(var.instance_type, terraform.workspace) 
+  #key_name = var.key_name
+  availability_zone = "us-east-1a"
+  #vpc_security_group_ids = [var.sg_id]
+  tags = { 
+    Name = local.default_name
+  }
+
+}
+
+resource "aws_ebs_volume" "my-vol" { 
+  availability_zone = "us-east-1a"
+  size = 40
+
+  tags = {
+    Name = "my-vol-ebs"
+   }
+
+}
+
+
+resource "aws_volume_attachment" "ebs_att" {
+   device_name = "/dev/sdh"
+   volume_id   = aws_ebs_volume.my-vol.id
+   instance_id = aws_instance.dev-app.id
+}
+
