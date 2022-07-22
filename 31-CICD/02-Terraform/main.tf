ad82e3180aa216cc9ab129c09e1be03d7ceb68f5
Amit Vashist <amitvashist7@outlook.com>
2022-07-22 11:47:18 +0000

    Tomcat Port : 8080

--- 31-CICD/02-Terraform/main.tf
+++ 31-CICD/02-Terraform/main.tf
@@ -62,6 +62,17 @@ resource "azurerm_network_security_group" "myterraformnsg" {
       destination_address_prefix = "*"
     }
 
+   security_rule {
+      name                       = "tomcat"
+      priority                   = 1005
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8080"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+
     tags = {
         environment = "Terraform Demo"
     }
